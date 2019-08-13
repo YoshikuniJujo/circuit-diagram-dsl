106842e6a0d7aa0ed99845b55cf186f437e18a5c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-13 13:46:20 +0900

    test hLineD

---
+++ test/tryHLine.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Diagrams.Prelude (mkWidth)
+import Diagrams.Backend.SVG (renderSVG)
+
+import Circuit.DiagramDsl
+
+main :: IO ()
+main = case sample `execDiagramMapM` 2 of
+	Right d -> renderSVG "hLine.svg" (mkWidth 600) $ drawDiagram d
+	Left emsg -> putStrLn $ "no diagram: " ++ emsg
+
+eid0, eid1, eid2, eid3, eid4, eid5 :: ElementId
+[eid0, eid1, eid2, eid3, eid4, eid5] = ["0", "1", "2", "3", "4", "5"]
+
+sample :: DiagramMapM ()
+sample = do
+	lp <- newElement0 eid0 hLineD
+	ip <- inputPosition lp
+	() <$ newElement eid1 hLineD ip
+	connectLine eid0 eid1
