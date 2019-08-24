438112ce0709238f7c7432919f4ab0715104a517
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-24 18:57:23 +0900

    more flexible distance: Dist -> AStarDist a

--- src/AStar/AStarMonad.hs
+++ src/AStar/AStarMonad.hs
@@ -24,44 +24,44 @@ switch _ y Close = y
 
 type Dist = Word
 
-type Node n = (Switch, (Dist, n))
+type Node d n = (Switch, (d, n))
 
-tag :: Node n -> Switch
+tag :: Node d n -> Switch
 tag = fst
 
-dist :: Node n -> Dist
+dist :: Node d n -> d
 dist = fst . snd
 
-node :: Node n -> n
+node :: Node d n -> n
 node = snd . snd
 
-type AStarM n = StateT (Moment n) Maybe
-type Moment n = (Heap (Node n), Map n n)
+type AStarM d n = StateT (Moment d n) Maybe
+type Moment d n = (Heap (Node d n), Map n n)
 
-runAStarM :: AStarM n a -> Maybe (a, Moment n)
+runAStarM :: AStarM d n a -> Maybe (a, Moment d n)
 runAStarM = (`runStateT` (H.empty, M.empty))
 
-putOpen :: Ord n => Dist -> n -> n -> AStarM n ()
+putOpen :: (Ord d, Ord n) => d -> n -> n -> AStarM d n ()
 putOpen d n pr = do
 	ms <- shortest n <$> gets fst
 	case ms of
 		Just s | s <= d -> return ()
 		_ -> putNode Open d n >> setParent n pr
 
-shortest :: Eq n => n -> Heap (Switch, (Dist, n)) -> Maybe Dist
+shortest :: (Ord d, Eq n) => n -> Heap (Switch, (d, n)) -> Maybe d
 shortest n = uncurry minMaybe
 	. ((dist <$>) . headHeap *** (dist <$>) . headHeap)
 	. H.partition ((== Open) . tag) . H.filter ((== n) . node)
 
-headNode :: AStarM n (Dist, n)
+headNode :: AStarM d n (d, n)
 headNode = do
 	(h, m) <- get
 	((tg, dn), h') <- lift $ uncons h
 	put (h', m)
 	switch (return dn) (lift Nothing) tg
 
-putNode :: Ord n => Switch -> Dist -> n -> AStarM n ()
+putNode :: (Ord d, Ord n) => Switch -> d -> n -> AStarM d n ()
 putNode t d n = modify . first $ H.insert (t, (d, n))
 
-setParent :: Ord n => n -> n -> AStarM n ()
+setParent :: Ord n => n -> n -> AStarM d n ()
 setParent c p = modify . second $ M.insert c p
